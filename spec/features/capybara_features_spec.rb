0dc3424a4f4209647b394d050801b80bcc3f2bd6
Max Van Aken <mtvanaken@gmail.com>
2019-05-16 14:52:23 +0100

    Added fight buttons and test

--- spec/features/capybara_features_spec.rb
+++ spec/features/capybara_features_spec.rb
@@ -1,9 +1,17 @@
 feature 'Player Names' do
   scenario 'see player names on screen' do
-    visit ('/')
-    fill_in :player_1_name, with: 'Bob'
-    fill_in :player_2_name, with: 'Dave'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Welcome Bob & Dave'
+  end
+
+  scenario "show player 2 hit points" do
+    sign_in_and_play
+    expect(page).to have_content 'HP'
+  end
+
+  scenario "click fight button" do
+    sign_in_and_play
+    click_link 'Attack'
+    expect(page).to have_content 'Bob attacked Dave'
   end
 end
