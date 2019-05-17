80358e3cf622679740a71e4f8a3daca7d4f3aa28
Max Van Aken <mtvanaken@gmail.com>
2019-05-17 14:17:20 +0100

    Closing: Implementing Hit Points

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,20 @@
 class Player
-  attr_reader :name
+  attr_reader :name, :hp
 
-  def initialize(name)
+  HIT_POINTS = 100
+  DAMAGE = 10
+  def initialize(name, hp = HIT_POINTS)
     @name = name
+    @hp = hp
+  end
+
+  def attack(player)
+    player.reduce_hit_points
+  end
+
+
+
+  def reduce_hit_points
+    @hp -= DAMAGE
   end
 end
--- spec/features/capybara_features_spec.rb
+++ spec/features/capybara_features_spec.rb
@@ -15,4 +15,10 @@ feature 'Player Names' do
     expect(page).to have_content 'Bob attacked Dave'
   end
 
+  scenario "attack reduces HP of player 2" do
+    sign_in_and_play
+    click_link 'Attack'
+    expect(page).to have_content 'Dave 90HP'
+  end
+
 end
